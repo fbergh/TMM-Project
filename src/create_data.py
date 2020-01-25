b96c70ca3cbe61dadeff4af3c9f981cbb35b5761
Bergh, F.J.P.N. van den (Freek) <f.vandenbergh@student.ru.nl>
2020-01-25 14:50:25 +0100

    Finished pre-processing; created pre-processed datasets for every research period

--- src/create_data.py
+++ src/create_data.py
@@ -1,63 +1,61 @@
-#%%
 # Import libraries
 import pandas as pd
 import os
-import sys
-from src.twitter_preprocessor import custom_preprocess
+from src.twitter_preprocessor import preprocess
+
 # See current working directory
 print(os.getcwd())
 
 # Initialise path to data
 DATA_PATH = "../data/"
 
-#%%
-tweets_to2018 = pd.read_csv(DATA_PATH+"output_got_2014-01-01.csv", sep=';')
-print(tweets_to2018.head())
-
-#%%
-tweets_from2018 = pd.read_csv(DATA_PATH+"output_got_2018-09-22.csv", sep=';')
-print(tweets_from2018.head())
-
-#%%
 def dateToISO8601(date):
     '''Converts date from "day/month/year"-format to "year/month/day"-format (ISO8601)'''
     day, month, year = date.split("/")
     return year+"/"+month+"/"+day
 
-#%%
-# Append the two CSV files and drop "geo","mentions","hashtags", and all "Unnamed:x" columns
-# Then sort by (reformatted) date and reset index and save as new file
-tweets_all = tweets_from2018.append(tweets_to2018, ignore_index=True)
-tweets_all = tweets_all.drop(tweets_all.columns[[0,1,2,3,4,7]], axis=1)
-# Remove hours and minutes and reformat date
-tweets_all["date"] = tweets_all["date"].apply(lambda s: s[:10]).apply(dateToISO8601)
-tweets_all.sort_values(by=["date"], inplace=True)
-tweets_all.reset_index(drop=True, inplace=True)
+def load_all_tweets():
+    tweets_to2018 = pd.read_csv(DATA_PATH+"output_got_2014-01-01.csv", sep=';')
+    tweets_from2018 = pd.read_csv(DATA_PATH+"output_got_2018-09-22.csv", sep=';')
+    # Append the two CSV files and drop "geo","mentions","hashtags", and all "Unnamed:x" columns
+    # Then sort by (reformatted) date and reset index and save as new file
+    tweets_all = tweets_from2018.append(tweets_to2018, ignore_index=True)
+    tweets_all = tweets_all.drop(tweets_all.columns[[0, 1, 2, 3, 4, 7]], axis=1)
+    # Remove hours and minutes and reformat date
+    tweets_all["date"] = tweets_all["date"].apply(lambda s: s[:10]).apply(dateToISO8601)
+    tweets_all.sort_values(by=["date"], inplace=True)
+    tweets_all.reset_index(drop=True, inplace=True)
+    return tweets_all
 
-#%%
-# Pre-process tweets
-tweets_all["text"] = tweets_all["text"].apply(custom_preprocess)
-tweets_all = tweets_all[tweets_all["text"].astype(bool)]
-print(tweets_all.head(10))
+def preprocess_tweets(tweets):
+    # Remove all retweets (tweets starting with RT)
+    tweets = tweets[~tweets.text.str.contains("(\s)?rt\s", na=False, regex=True)]
+    tweets["text"] = tweets["text"].apply(preprocess)
+    # Drop empty tweets
+    tweets = tweets[tweets["text"].astype(bool)]
+    tweets = tweets.drop_duplicates(subset="text")
+    return tweets
 
-#%%
-# Finally export the CSV file containing all pre-processed tweets
-tweets_all.to_csv(DATA_PATH+"tweets_all.csv")
+def main():
+    tweets_all = load_all_tweets()
+    tweets_all = preprocess_tweets(tweets_all)
+    # Export the CSV file containing all pre-processed tweets
+    tweets_all.to_csv(DATA_PATH+"tweets_all.csv")
 
-#%%
-# Create CSV files for every time period I wish to research
-# 2015 and before
-tweets_2015bf = tweets_all.loc[tweets_all['date'] < "2016"]
-tweets_2015bf.to_csv(DATA_PATH+"tweets_2015-and-before.csv")
-print(tweets_2015bf.head())
-#%%
-# 2016 - 14/10/17
-tweets_2016to20171014 = tweets_all.loc[(tweets_all['date'] < "2017/10/14") 
-                                     & (tweets_all['date'] > "2016")]
-tweets_2016to20171014.to_csv(DATA_PATH+"tweets_2016-to-20171014.csv")
-print(tweets_2016to20171014.head())
-#%%
-# 15/10/17 - 31/11/19
-tweets_20171015to20191131 = tweets_all.loc[tweets_all['date'] > "2017/10/14"]
-tweets_20171015to20191131.to_csv(DATA_PATH+"tweets_20171015-to-20191131.csv")
-print(tweets_20171015to20191131.head())
+    # Create CSV files for every time period I wish to research
+    # 2015 and before
+    tweets_period1 = tweets_all.loc[tweets_all['date'] < "2016"]
+    tweets_period1.to_csv(DATA_PATH+"tweets_period1.csv")
+    print(tweets_period1.head(), tweets_period1.shape)
+    # 2016 - 14/10/17
+    tweets_period2 = tweets_all.loc[(tweets_all['date'] < "2017/10/14")
+                                        & (tweets_all['date'] > "2016")]
+    tweets_period2.to_csv(DATA_PATH+"tweets_period2.csv")
+    print(tweets_period2.head(), tweets_period2.shape)
+    # 15/10/17 - 31/11/19
+    tweets_period3 = tweets_all.loc[tweets_all['date'] > "2017/10/14"]
+    tweets_period3.to_csv(DATA_PATH+"tweets_period3.csv")
+    print(tweets_period3.head(), tweets_period3.shape)
+
+if __name__ == '__main__':
+    main()
--- src/twitter_preprocessor.py
+++ src/twitter_preprocessor.py
@@ -7,96 +7,48 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk import re
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
-MIN_YEAR = 1900
-MAX_YEAR = 2100
-
-
-def get_url_patern():
-    return re.compile(
-        r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))'
-        r'[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})')
-
-
-def get_emojis_pattern():
-    try:
-        # UCS-4
-        emojis_pattern = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
-    except re.error:
-        # UCS-2
-        emojis_pattern = re.compile(
-            u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
-    return emojis_pattern
-
+wnl = WordNetLemmatizer()
 
 def get_hashtags_pattern():
     return re.compile(r'#\w*')
 
-
 def get_single_letter_words_pattern():
     return re.compile(r'(?<![\w\-])\w(?![\w\-])')
-
-
-def get_blank_spaces_pattern():
-    return re.compile(r'\s{2,}|\t')
-
-
-def get_twitter_reserved_words_pattern():
-    return re.compile(r'(RT|rt|FAV|fav|VIA|via)')
-
 
 def get_mentions_pattern():
     return re.compile(r'@\w*')
 
-
-def is_year(text):
-    if (len(text) == 3 or len(text) == 4) and (MIN_YEAR < len(text) < MAX_YEAR):
-        return True
-    else:
-        return False
-
-def custom_preprocess(text):
-    return remove_stopwords(remove_blank_spaces(remove_single_letter_words(remove_punctuation(
-                            remove_twitter_reserved_words(remove_hashtags(remove_mentions(remove_urls(text))))))))
-
-def fully_preprocess(text):
-        return remove_urls(text) \
-            .remove_mentions(text) \
-            .remove_hashtags(text) \
-            .remove_twitter_reserved_words(text) \
-            .remove_punctuation(text) \
-            .remove_single_letter_words(text) \
-            .remove_blank_spaces(text) \
-            .remove_stopwords(text) \
-            .remove_numbers(text)
-
-def remove_urls(text):
-    return re.sub(pattern=get_url_patern(), repl='', string=text)
-
-
-def remove_punctuation(text):
-    return text.translate(str.maketrans('', '', string.punctuation))
-
+def preprocess(text):
+    text = text.lower()
+    text = remove_mentions(text)
+    text = remove_hashtags(text)
+    text = remove_punctuation(text)
+    text = remove_single_letter_words(text)
+    text = remove_alphanumeric_strings(text)
+    text = remove_urls(text)
+    text = remove_stopwords(text)
+    return lemmatise(text)
 
 def remove_mentions(text):
     return re.sub(pattern=get_mentions_pattern(), repl='', string=text)
 
-
 def remove_hashtags(text):
     return re.sub(pattern=get_hashtags_pattern(), repl='', string=text)
 
-
-def remove_twitter_reserved_words(text):
-    return re.sub(pattern=get_twitter_reserved_words_pattern(), repl='', string=text)
-
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
 
 def remove_single_letter_words(text):
     return re.sub(pattern=get_single_letter_words_pattern(), repl='', string=text)
 
+def remove_alphanumeric_strings(text):
+    return re.sub(r'[a-zA-Z]+\d{2,}|\d{10,}|[a-zA-Z0-9]{20,}', '', text)
 
-def remove_blank_spaces(text):
-    return re.sub(pattern=get_blank_spaces_pattern(), repl=' ', string=text)
-
+def remove_urls(text):
+    return re.sub(r'pictwitter[a-zA-Z0-9]*|http(s)?(www)?\s[a-zA-Z0-9]*', '', text)
 
 def remove_stopwords(text, extra_stopwords=None):
     if extra_stopwords is None:
@@ -110,19 +62,7 @@ def remove_stopwords(text, extra_stopwords=None):
             new_sentence.append(w)
     return ' '.join(new_sentence)
 
-
-def remove_numbers(text, preserve_years=False):
-    text_list = text.split(' ')
-    for text in text_list:
-        if text.isnumeric():
-            if preserve_years:
-                if not is_year(text):
-                    text_list.remove(text)
-            else:
-                text_list.remove(text)
-
-    return ' '.join(text_list) 
-
-
-def lowercase(text):
-    return text.lower()
+def lemmatise(text):
+    words = word_tokenize(text)
+    lemmatised_words = [wnl.lemmatize(word, pos="v") for word in words]
+    return " ".join(lemmatised_words)
